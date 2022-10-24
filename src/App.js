858b6d0428ed438dfc98d747265a3d664225760d
siddharthkanna <thesiddharthk@gmail.com>
2022-10-24 10:00:53 +0530

    bd test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Intro />
+      <Intro data={resumeData.main} />
       <About />
       {/*<Skills />*/}
       {/*<Projects />*/}
