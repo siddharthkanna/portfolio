b93b5129bd9aecfc7b9ef4023d0273f3b0ad252c
siddharthkanna <thesiddharthk@gmail.com>
2022-10-25 17:43:54 +0530

    done with about,minor changes in intro and nav

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./About.css";
+import sid from "./sid.png";
 
 function About() {
   return (
     <div className="about">
-      <h1>02.About me</h1>
-      <p>
-        Hello! I'm Siddharth, Junior Frontend Developer,based in Hyderabad.I enjoy
-        creating beautiful and reliable applications for internet and phones. My
-        goal is to always build scalable products and performant experiences.
-      </p>
-
+      <div className="a-des">
+        <h1 className="a-title">02.About me</h1>
+        <p>
+          Hello! I'm Siddharth, Junior Frontend Developer,based in Hyderabad.I
+          enjoy creating beautiful and reliable applications for internet and
+          phones. My goal is to always build scalable products and performant
+          experiences.
+        </p>
+      </div>
+      <div className="a-img">
+        <img src={sid} alt="sid" width="260px" height="300px" />
+      </div>
     </div>
   );
 }
