21a0a4a22e9e7a2d6cfba19f0265a70956d6aea5
siddharthkanna <thesiddharthk@gmail.com>
2022-11-01 11:27:36 +0530

    remove reveal fade

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./About.css";
 import sid from "./sid.png";
-import { Fade } from "react-awesome-reveal";
+
 
 function About() {
   return (
-    <Fade delay={500}>
+    
       <div className="about" id="aboutt">
         <div className="a-des">
           <h1 className="a-title">02.About me</h1>
@@ -22,7 +22,7 @@ function About() {
           <img src={sid} alt="sid" width="260px" height="300px" />
         </div>
       </div>
-    </Fade>
+    
   );
 }
 
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,34 +2,29 @@ import React from "react";
 import SingleProject from "./SingleProject";
 import "./Projects.css";
 import { projectsData } from "./projectsData";
-import { Fade } from "react-awesome-reveal";
 
 function Projects() {
   return (
-   
-      <div className="projects" id="project">
-        <div className="projects__header">
-          <h1>03.Projects</h1>
-        </div>
-        <div className="projects__body">
-          <div className="projects__bodyContainer">
-            {projectsData.map((project) => (
-               <Fade delay={500}>
-              <SingleProject
-                key={project.id}
-                name={project.projectName}
-                desc={project.projectDesc}
-                tags={project.tags}
-                code={project.code}
-                demo={project.demo}
-                image={project.image}
-              />
-              </Fade>
-            ))}
-          </div>
+    <div className="projects" id="project">
+      <div className="projects__header">
+        <h1>03.Projects</h1>
+      </div>
+      <div className="projects__body">
+        <div className="projects__bodyContainer">
+          {projectsData.map((project) => (
+            <SingleProject
+              key={project.id}
+              name={project.projectName}
+              desc={project.projectDesc}
+              tags={project.tags}
+              code={project.code}
+              demo={project.demo}
+              image={project.image}
+            />
+          ))}
         </div>
       </div>
-   
+    </div>
   );
 }
 
