3c49cc39f2780f92cf2444a0d6872135f038155c
siddharthkanna <thesiddharthk@gmail.com>
2022-11-05 02:21:28 +0530

    added fb database (contact form)

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,28 +1,99 @@
 import React from "react";
 import "./Contact.css";
+import { useState } from "react";
+
 function Contact() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  let name, value;
+  const getUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+
+    const { name, email, message } = user;
+
+    if (name && email && message) {
+      const res = await fetch(
+        "https://portfolio-5d240-default-rtdb.firebaseio.com/ContactData.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            message,
+          }),
+        }
+      );
+
+      if (res) {
+        setUser({
+          name: "",
+          email: "",
+          message: "",
+        });
+        alert("Details Submitted successfully");
+      }
+    } else {
+      alert("Please fill the details properly");
+    }
+  };
   return (
     <div>
-      <form>
+      <form method="POST">
         <div className="title">
           <h2>05.Contact</h2>
         </div>
         <div className="half">
           <div className="item">
             <label for="name">NAME</label>
-            <input type="text" id="name" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={user.name}
+              onChange={getUserData}
+              required
+            />
           </div>
           <div class="item">
             <label for="email">EMAIL</label>
-            <input type="text" id="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={user.email}
+              onChange={getUserData}
+              required
+            />
           </div>
         </div>
         <div class="full">
           <label for="message">MESSAGE</label>
-          <textarea name="" id="message"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            value={user.message}
+            onChange={getUserData}
+            required
+          ></textarea>
         </div>
         <div className="action">
-          <input type="submit" value="SEND MESSAGE" />
+          <button type="submit" onClick={postData}>
+            SEND MESSAGE
+          </button>
         </div>
       </form>
     </div>
